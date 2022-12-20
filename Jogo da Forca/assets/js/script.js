92707cea3ac8837ace14c8a30901e5bb8e3e78e0
annavoigg <gomesgiovanna006@gmail.com>
2022-12-20 15:33:48 -0300

    atualização - função de checar caractre

--- Jogo da Forca/assets/js/script.js
+++ Jogo da Forca/assets/js/script.js
@@ -1,106 +1,108 @@
-// seletores 
+// seletores
 
-let tabuleiro = document.getElementById('forca').getContext('2d');
+let tabuleiro = document.getElementById("forca").getContext("2d");
 
-let palavras = ["PARQUE", "PETECA", "CORDA", "CARIMBO", "FERRO", "CORPE", "PEDRA", "FOGO", "AR", "DESIGN", "ARTE", "JOGOS", "BOLA"];
+let palavras = [
+  "PARQUE",
+  "PETECA",
+  "CORDA",
+  "CARIMBO",
+  "FERRO",
+  "CORPE",
+  "PEDRA",
+  "FOGO",
+  "AR",
+  "DESIGN",
+  "ARTE",
+  "JOGOS",
+  "BOLA",
+];
 
 let palavraSecreta = "";
 let letras = [];
 
 let erros = 6;
 
+let canvas = document.getElementById("canvas");
 
+function sorteiaPalavras() {
+  let palavra = palavras[Math.floor(Math.random() * palavras.length)];
 
-let canvas = document.getElementById('canvas');
-
-function sorteiaPalavras(){
-    let palavra = palavras[Math.floor(Math.random() * palavras.length)];
-
-    palavraSecreta = palavra;
-    return palavraSecreta;
+  palavraSecreta = palavra;
+  return palavraSecreta;
 }
 
-function verificarLetra(key){
+function verificarLetra(key) {
+  let estado = false;
 
-    let estado = false;
-
-    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){
-
-        letras.push(key);
-        console.log(key);
-        console.log(letras)
-        return estado
-    } else{
-        estado = true
-        letras.push(key);
-        console.log(key);
-        console.log(letras, "if true")
-        return estado
-    }
+  if (
+    (key >= 65 && letras.indexOf(key)) ||
+    (key <= 90 && letras.indexOf(key))
+  ) {
+    letras.push(key);
+    console.log(key);
+    console.log(letras);
+    return estado;
+  } else {
+    estado = true;
+    letras.push(key);
+    console.log(key);
+    console.log(letras, "if true");
+    return estado;
+  }
 }
 
-function adicionarLetraIncorreta(){
-
-    erros -= 1
+function adicionarLetraIncorreta() {
+  erros -= 1;
 }
 
+function iniciarJogo() {
+  document.getElementById("temporario").style.display = "none";
+  canvas.classList.remove("ativo");
 
+  sorteiaPalavras();
+  desenharCanvas();
+  desenhaTracinhos();
+  // desenhaLetras();
 
+  document.onkeydown = (e) => {
+    let letraDigitada = e.key.toUpperCase();
 
-function iniciarJogo(){
-    
-    document.getElementById('temporario').style.display = "none";
-    canvas.classList.remove("ativo")
-
-    sorteiaPalavras();
-    desenharCanvas()
-    desenhaTracinhos();
-    // desenhaLetras();
-    
-    document.onkeydown = (e) =>{
-
-        let letraDigitada = e.key.toUpperCase();
-    
-        if(verificarLetra(letraDigitada) && palavraSecreta.includes(letraDigitada)){
-    
-            for(let i = 0; i < palavraSecreta.length; i++){
-    
-                if(palavraSecreta[i] === letraDigitada){
-                    escreverLetraCorreta(i);
-            
-                } 
-
-            }
-
-        } else {
-    
-            adicionarLetraIncorreta(letraDigitada);
-            escreverLetraIncorreta(letraDigitada, erros);
-            desenhaCorpo(erros);
+    if (
+      verificarLetra(letraDigitada) &&
+      palavraSecreta.includes(letraDigitada)
+    ) {
+      for (let i = 0; i < palavraSecreta.length; i++) {
+        if (palavraSecreta[i] === letraDigitada) {
+          escreverLetraCorreta(i);
         }
+      }
+    } else if (!checkChar(e)) {
+      e.preventDefault();
+      alert("Use apenas letras.");
+    } else {
+      adicionarLetraIncorreta(letraDigitada);
+      escreverLetraIncorreta(letraDigitada, erros);
+      desenhaCorpo(erros);
     }
 
+  };
 }
-
-
 
 // captar a tecla digitada
 
+function checkChar(e) {
+  const char = String.fromCharCode(e.keyCode);
 
+  const padrao = "[a-zA-Z]";
 
+  if (char.match(padrao)) {
+    console.log(char);
+    return true;
+  }
+}
 
-
-// escrever letra digitada 
-
-
-
-
-
-
-
-
-
-
+// escrever letra digitada
 
 //  if (textoComum.value.trim() == " " || textoComum.value.trim() == ""){
 //      alert("É necessário digitar uma mensagem.")
@@ -109,7 +111,6 @@ function iniciarJogo(){
 //      garnet.classList.remove("ativo")
 //      caixaTexto.classList.remove("ativo")
 //  };
-
 
 // function checkChar(e) {
 //     const char = String.fromCharCode(e.keyCode);
