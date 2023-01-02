3216e2e2939a4d723b3f8f90889baf8e73e5bb9c
annavoigg <gomesgiovanna006@gmail.com>
2023-01-02 19:24:14 -0300

    atualização - resolvendo bugs

--- Jogo da Forca/assets/js/perdeOuGanha.js
+++ Jogo da Forca/assets/js/perdeOuGanha.js
@@ -1,4 +1,4 @@
-const botaoNovoJogo = document.querySelector(".modal__voltaInicio")
+const botaoNovoJogo = document.querySelector(".modal__voltaInicio");
 
 const modalDerrota = document.getElementById("modalDerrota");
 const fadeDerrota = document.getElementById("fadeDerrota");
@@ -7,46 +7,29 @@ const modalVitoria = document.getElementById("modalVitoria");
 const fadeVitoria = document.getElementById("fadeVitoria");
 
 const disparaDerrota = () => {
-    [modalDerrota, fadeDerrota].forEach((el) => el.classList.toggle("hide__derrota"));
+  [modalDerrota, fadeDerrota].forEach((el) =>
+    el.classList.toggle("hide__derrota")
+  );
 
-    document.onkeydown = (e) => {
-        e.preventDefault();
-        return
-    }
+  document.onkeydown = (e) => {
+    e.preventDefault();
+    return;
+  };
 };
 
 function voltarInicio() {
-    window.location.href = 'index.html'
+  window.location.href = "index.html";
 }
 
 function disparaVitoria() {
+  const resultado = palavraSecreta.map((letra) => {
+    return letrasCorretas.includes(letra);
+  });
 
-    const resultado =
-        palavraSecreta.map(letra => {
-            return letrasCorretas.includes(letra);
-        })
+  if (!resultado.includes(false)) {
+    [modalVitoria, fadeVitoria].forEach((el) =>
+      el.classList.remove("hide__vitoria")
+    );
 
-    // console.log(resultado);
-
-    if (!(resultado.includes(false))) {
-        [modalVitoria, fadeVitoria].forEach((el) => el.classList.toggle("hide__vitoria"));
-    }
-
-    // if (resultado.includes(false)) {
-    //     console.log("Ainda não ganhou!");
-    // } else {
-    //     [modalVitoria, fadeVitoria].forEach((el) => el.classList.toggle("hide__vitoria"));
-    // }
-
-    //     for (let i = 0; i < palavraSecreta.length; i++) {
-    //     if (letrasCorretas[i] == palavraSecreta[i]) {
-    //         [modalVitoria, fadeVitoria].forEach((el) => el.classList.toggle("hide__vitoria"));
-
-    //         document.onkeydown = (e) =>{
-    //             e.preventDefault();
-    //             return;
-    //         }
-    //     }
-    // }
-
+  }
 }
